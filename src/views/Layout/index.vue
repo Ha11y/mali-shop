89838e987360b6e6ee2a2558f23e29ff6bad0c1f
bird <491303100@qq.com>
2023-07-04 20:40:57 +0800

    实现吸顶导航交互

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,11 +2,14 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutStick from './components/LayoutStick.vue'
 </script>
 
 <template>
+  <LayoutStick/>
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
   <LayoutFooter />
+
 </template>

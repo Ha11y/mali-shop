c82281291777f25ff853861def0c303f57f57a9e
bird <491303100@qq.com>
2023-07-04 16:27:54 +0800

    做了axios的实例和基本配置

--- src/main.js
+++ src/main.js
@@ -7,7 +7,11 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+//测试接口函数
+import { getCategory } from './apis/testApi'
+getCategory().then(res=>{
+    console.log(res)
+})
 app.use(createPinia())
 app.use(router)
 

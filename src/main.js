f9d4bb4228a805933718bcd263a2aa78c50e26c3
bird <491303100@qq.com>
2023-07-04 17:22:03 +0800

    更换主题色,引入自定义样式文件

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './assets/main.css'
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import '@/styles/common.scss'
 const app = createApp(App)
 //测试接口函数
 import { getCategory } from './apis/testApi'

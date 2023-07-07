509fefbafa253f06d607597082878dbf8f7a47e7
bird <491303100@qq.com>
2023-07-07 17:58:22 +0800

    购物车删除功能的实现

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -19,9 +19,14 @@ export const useCartStore= defineStore('cart',()=>{
        
       }
     }
+    const delCart =(skuId)=>{
+       const idx= cartList.value.findIndex((item)=>skuId===item.skuId)
+        cartList.value.splice(idx,1)
+    }
     return {
         cartList,
-        addCart
+        addCart,
+        delCart
     }
 },
 {

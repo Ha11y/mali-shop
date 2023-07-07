ba67a0b5a6910c42903329d7f563a3ea86dac805
bird <491303100@qq.com>
2023-07-07 15:46:21 +0800

    在axios请求拦截器中携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import {useuserStore} from '@/stores/user'
+
  const httpInstance= axios.create({
     baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout:5000
@@ -10,6 +12,12 @@ import 'element-plus/theme-chalk/el-message.css'
 
 //axios请求拦截器
 httpInstance.interceptors.request.use(config=>{
+    //从pinia 中获取token数据
+    const userStore =useuserStore()
+    const token=userStore.userInfo.token
+    if(token){
+        config.headers.Authorization=`Bearer ${token}`
+    }
     return config
 },e=>Promise.reject(e))
 

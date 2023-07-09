586dc276e2ab8dc10563d27a980a9c233aee9639
bird <491303100@qq.com>
2023-07-09 16:14:20 +0800

    个人中心，我的订单,三级路由的配置

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -29,7 +32,7 @@ const router = createRouter({
         component:SubCategory
       },
       {
-        path:'/detail/:id',
+        path:'detail/:id',
         component:Detail
        },
        {
@@ -37,17 +40,31 @@ const router = createRouter({
         component:CartList
        },
        {
-        path:'/checkout',
+        path:'checkout',
         component:Checkout
        },
        {
-        path:'/pay',
+        path:'pay',
         component:Pay
        },
        {
         path: 'paycallback', // 注意路径，必须是paycallback
         component: PayBack
       },
+      {
+        path:'member',
+        component:Member,
+        children:[
+          {
+            path:'user',
+            component:UserInfo
+          },
+          {
+            path:'order',
+            component:UserOrder
+          }
+        ]
+      }
     ]
    },
    {
